93a4868e23b93149c26232b85f9f1f7dfde94b6b
abnunez <adam.b.nunez@gmail.com>
2018-06-13 11:07:58 -0500

    git ignore

---
+++ Assets/game/core.clj
@@ -0,0 +1,11 @@
+(ns game.core
+  (:use arcadia.core arcadia.linear)
+  (:import [UnityEngine Collider2D Physics
+            GameObject Input Rigidbody2D
+            Vector2 Mathf Resources Transform
+            Collision2D Physics2D]
+           ArcadiaState))
+
+(def game-start []
+  "Link start."
+  )
